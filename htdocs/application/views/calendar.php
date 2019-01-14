04a9851bceca44794eb7a7381fe34b5d241d329d
zukri01 <zukri@outlook.com>
2019-01-14 16:27:42 +0700

    Update calendar.php

--- htdocs/application/views/calendar.php
+++ htdocs/application/views/calendar.php
@@ -52,85 +52,69 @@ $this->load->view('template/menu');
                                 </div>
                                 <!-- place -->
                                 <div id="calendarIO"></div>
-                                <div class="modal fade" id="create_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-                                    <div class="modal-dialog" role="document">
+
+                                <!-- end place -->
+								<div id="create_modal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
+                                    <div class="modal-dialog">
                                         <div class="modal-content">
-                                            <form class="form-horizontal" method="POST" action="POST" id="form_create">
-                                                <input type="hidden" name="calendar_id" value="0">
-                                                <div class="modal-header">
-                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                      <span aria-hidden="true">&times;</span>
-                                                    </button>
-                                                    <h4 class="modal-title" id="myModalLabel">Create calendar event</h4>
-                                                </div>
-                                                <div class="modal-body">
-
+										<form class="form-horizontal" method="POST" action="POST" id="form_create">
+										<input type="hidden" name="calendar_id" value="0">
+                                            <div class="modal-header">
+                                                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                                                <h4 class="modal-title">Tambah Kegiatan</h4>
+                                            </div>
+                                            <div class="modal-body">
                                                     <div class="form-group">
-                                                         <div class="alert alert-danger" style="display: none;"></div>
+                                                        <label class="control-label">Kegiatan <span class="required"> * </span></label>
+                                                        <input type="text" name="title" class="form-control" placeholder="Kegiatan">                                                       
                                                     </div>
-                                                    <div class="form-group">
-                                                        <label class="control-label col-sm-2">Title  <span class="required"> * </span></label>
-                                                        <div class="col-sm-10">
-                                                            <input type="text" name="title" class="form-control" placeholder="Title">
-                                                        </div>
+													<div class="form-group">
+														<label class="control-label">Lokasi <span class="required"> * </span></label>
+                                                        <textarea name="description" rows="3" class="form-control"  placeholder="Lokasi Kegiatan"></textarea>                                                       
                                                     </div>
-
-                                                    <div class="form-group">
-                                                        <label class="control-label col-sm-2">Description</label>
-                                                        <div class="col-sm-10">
-                                                            <textarea name="description" rows="3" class="form-control"  placeholder="Enter description"></textarea>
-                                                        </div>
-                                                    </div>
-
-                                                    <div class="form-group">
-                                                        <label for="color" class="col-sm-2 control-label">Color</label>
-                                                        <div class="col-sm-10">
+													<div class="form-group">
+														<label for="color" class="control-label">Disposisi</label>
                                                             <select name="color" class="form-control">
-                                                                <option value="">Choose</option>
-                                                                <option style="color:#0071c5;" value="#0071c5">&#9724; Dark blue</option>
-                                                                <option style="color:#40E0D0;" value="#40E0D0">&#9724; Turquoise</option>
-                                                                <option style="color:#008000;" value="#008000">&#9724; Green</option>                       
-                                                                <option style="color:#FFD700;" value="#FFD700">&#9724; Yellow</option>
-                                                                <option style="color:#FF8C00;" value="#FF8C00">&#9724; Orange</option>
-                                                                <option style="color:#FF0000;" value="#FF0000">&#9724; Red</option>
-                                                                <option style="color:#000;" value="#000">&#9724; Black</option>
+                                                                <option value="">Disposisi</option>
+                                                                <option style="color:#0071c5;" value="#0071c5">&#9724; Walikota</option>
+                                                                <option style="color:#40E0D0;" value="#40E0D0">&#9724; Wawako</option>
+                                                                <option style="color:#008000;" value="#008000">&#9724; Asisten Pemerintahan</option>                       
+                                                                <option style="color:#FFD700;" value="#FFD700">&#9724; Asisten </option>
+                                                                <option style="color:#FF8C00;" value="#FF8C00">&#9724; Staf Ahli</option>
+                                                                <option style="color:#FF0000;" value="#FF0000">&#9724; Staf Ahli Bagian hukum</option>
+                                                                <option style="color:#000;" value="#000">&#9724; Kabag</option>
                                                             </select>
-                                                        </div>
-                                                    </div>
-
-                                                    <div class="form-group">
-                                                        <label class="control-label col-sm-2">Start Date</label>
-														<div class="col-sm-10">
+													</div>
+													<div class="form-group">
+                                                        <label class="control-label">Mulai</label>														
 															<div class="input-append date form_datetime" data-date="2013-02-21T15:25:00Z">
 																<input size="16" type="text" value="" readonly>
-																<span class="add-on"><i class="icon-remove"></i></span>
-																<span class="add-on"><i class="icon-calendar"></i></span>
+																<span class="add-on"><i class="mdi-close"></i></span>
+																<span class="add-on"><i class="mdi-calendar-clock"></i></span>
 															</div>
-														</div>
                                                     </div>
-
-                                                    <div class="form-group">
-                                                        <label class="control-label col-sm-2">End Date</label>
-                                                        <div class="col-sm-10">
+													<div class="form-group">
+                                                        <label class="control-label">Mulai</label>														
 															<div class="input-append date form_datetime" data-date="2013-02-21T15:25:00Z">
 																<input size="16" type="text" value="" readonly>
-																<span class="add-on"><i class="icon-remove"></i></span>
-																<span class="add-on"><i class="icon-calendar"></i></span>
+																<span class="add-on"><i class="mdi-close"></i></span>
+																<span class="add-on"><i class="mdi-calendar-clock"></i></span>
 															</div>
-                                                        </div>
-                                                    </div>
+                                                    </div>                                         
+                                            </div>
+                                            <div class="modal-footer">
+                                                <a href="javascript::void" class="btn btn-default waves-effect" data-dismiss="modal">Batal</a>
+                                                <button type="submit" class="btn btn-danger waves-effect waves-light">Simpan</button>
+												<a class="btn btn-danger delete_calendar" style="display: none;">Delete</a>
+												
 
+                                                    
                                                 </div>
-                                                <div class="modal-footer">
-                                                    <a href="javascript::void" class="btn default" data-dismiss="modal">Cancel</a>
-                                                    <a class="btn btn-danger delete_calendar" style="display: none;">Delete</a>
-                                                    <button type="submit" class="btn green">Submit</button>
-                                                </div>
-                                            </form>
+                                            </div>
+										</form>
                                         </div>
                                     </div>
                                 </div>
-                                <!-- end place -->
                             </div>
                         </div>
                         
