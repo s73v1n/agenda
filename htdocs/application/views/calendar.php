e5929a7804020dd65bc15b149abb695331751424
zukri01 <zukri@outlook.com>
2019-01-14 11:48:33 +0700

    Update calendar.php

--- htdocs/application/views/calendar.php
+++ htdocs/application/views/calendar.php
@@ -164,7 +164,7 @@ $this->load->view('template/menu');
                     center: 'title',
                     right: 'listMonth, agendaWeek, agendaDay'
                 },
-                defaultDate: moment().format('YYYY-MM-DD'),
+                defaultDate: moment().format('YYYY MM DD'),
                 
 				slotDuration: '00:30:00',  
 				minTime: '06:00:00',
@@ -175,8 +175,8 @@ $this->load->view('template/menu');
                 selectable: true,
                 selectHelper: true,
                 select: function(start, end) {
-                    $('#create_modal input[name=start_date]').val(moment(start).format('YYYY-MM-DD'));
-                    $('#create_modal input[name=end_date]').val(moment(end).format('YYYY-MM-DD'));
+                    $('#create_modal input[name=start_date]').val(moment(start).format('YYYY MM DD'));
+                    $('#create_modal input[name=end_date]').val(moment(end).format('YYYY MM DD'));
                     $('#create_modal').modal('show');
                     save();
                     $('#calendarIO').fullCalendar('unselect');
